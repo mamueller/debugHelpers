e619a93c1164e004f1c52e94439133c18fa7014b
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-12-19 01:07:20 +0100

    new version of pe that basically makes also pp obsolete

--- pkg/R/namedPlot.R
+++ pkg/R/namedPlot.R
@@ -1,10 +1,11 @@
 
 # vim:set ff=unix expandtab ts=2 sw=2:
 
-namedPlot=function(lexp,env){
+namedPlot=function(lexp,env=parent.frame()){
   #get the name of the caller
-  print(sys.calls())
+  pe(quote(sys.calls()))
   fileName=paste(as.character(sys.calls()[[sys.nframe()-1]]),"pdf",sep=".")
+  pp('fileName')
   plotAndCheck(fileName,lexp,env)
 }
 
--- pkg/R/pe.R
+++ pkg/R/pe.R
@@ -1,11 +1,10 @@
-pe <-
-function(string,env=parent.frame()){
+pe <- function(arg,env=parent.frame()){
 	### This function is used to print out a value of an quoted expression together with it and is helpful for debugging
   cat("\n########################################################################\n")
   callingFun <-as.list(sys.call(-1))[[1]]
   callerName<-toString(callingFun)
-  res <- toString(eval(string,env))
-  out <- sprintf('pe in %s:\nExpression\t: %s\nResult\t\t: %s \n',callerName,as.expression(string),res)
+  out <- sprintf('pe in %s:\nExpression\t: %s\nResult\t\t: \n',callerName,toString(substitute(arg)))
   cat(out)
+  print(arg)
   cat("\n########################################################################\n")
 }
