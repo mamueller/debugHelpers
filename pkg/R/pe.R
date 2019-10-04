ffede1b555e9cf3edc852293d8cd3ef65eb43a4f
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-10-04 04:43:11 +0200

    pp and pe now also work if called from  the top level environment

--- pkg/R/pe.R
+++ pkg/R/pe.R
@@ -1,10 +1,15 @@
 pe <- function(arg,env=parent.frame()){
 	### This function is used to print out a value of an quoted expression together with it and is helpful for debugging
   cat("\n########################################################################\n")
-  callingFun <-as.list(sys.call(-1))[[1]]
-  callerName<-toString(callingFun)
-  out <- sprintf('pe in %s:\nExpression\t: %s\nResult\t\t: \n',callerName,toString(substitute(arg)))
-  cat(out)
-  print(arg)
-  cat("\n########################################################################\n")
+  p_ind<-(sys.parent(n=1))
+	if(p_ind==0){
+	  print(get(string,env))
+  }else{
+    callingFun <-as.list(sys.call(-1))[[1]]
+    callerName<-toString(callingFun)
+    out <- sprintf('pe in %s:\nExpression\t: %s\nResult\t\t: \n',callerName,toString(substitute(arg)))
+    cat(out)
+    print(arg)
+    cat("\n########################################################################\n")
+    }
 }
--- pkg/R/pp.R
+++ pkg/R/pp.R
@@ -5,13 +5,14 @@ pp <- function(# print out a visible message
   env=parent.frame()
 ){
 	### This function is used to print out a value of a variable together with its name and is helpful for debugging
-  cat("\n############################### mm ########################################\n")
-	sc <- sys.call()
-	if(!is.null(sc)){
+  cat("\n############################### pp ########################################\n")
+  p_ind<-(sys.parent(n=1))
+	if(p_ind==0){
+	  print(get(string,env))
+  }else{
+    print(p_ind)
 	  callingFun <-as.list(sys.call(-1))[[1]]
 	  callerName<-toString(callingFun)
 	  print(paste("pp in",callerName,string,":="))
 	}
-  print(parent.frame())
-	print(get(string,env))
 }
